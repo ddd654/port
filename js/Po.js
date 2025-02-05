02696459e2e76743eb55189927dde671a2ac3bcd
bound654 <bound654@naver.com>
2025-02-05 19:24:01 +0900

    스크롤 슬라이드 만들기1

--- js/Po.js
+++ js/Po.js
@@ -40,47 +40,70 @@
 
 // var member = ['apple', 'banana', 'kiwi']
 
+
+//슬라이드를 마우스로 이동하는 기능
 let currentSlide = 0;
 const slides = document.querySelector(".slides");
 const sections = document.querySelectorAll("section");
 const totalSlides = sections.length;
 
-let isScrolling = false;
+let isScrolling = false; //스크롤 가능한지 상태
 
+//마우스로 스크롤 가능하게하는 기능능
 window.addEventListener('wheel', (event) => {
 
-    if (isScrolling) {
+    if (isScrolling) { //스크롤이 가능하면 끝낸다
         return;
     }
     isScrolling = true;
 
-    event.preventDefault();
+    event.preventDefault(); //기본 브라우저 동작막기?
 
-    console.log("스크롤 이벤트 확인");
-
-
-
-    if (event.deltaY > 0) {
-        // 아래로 스크롤 (다음 슬라이드로)
+    if (event.deltaY > 0) { // 아래로 스크롤하면
         if (currentSlide < totalSlides - 1) {
             currentSlide++;
         }
     } else {
-        // 위로 스크롤 (이전 슬라이드로)
+        //위로 스크롤하면
         if (currentSlide > 0) {
             currentSlide--;
         }
     }
 
-    // 슬라이드 이동
+    // 슬라이드 이동 효과 넣기
     slides.style.transition = "transform 0.5s ease-in-out"; // 애니메이션 효과 추가
-    slides.style.transform = `translateX(-${currentSlide * 100}vw)`; // 슬라이드 이동
+    slides.style.transform = `translateX( -${currentSlide * 100}vw )`; // 슬라이드 이동
 
-    //스크롤 후
-    setTimeout( () => {
+    //슬라이드 번호
+    console.log(`스크롤 중 슬라이드 번호는 ${currentSlide +1}`);
+
+    //스크롤 지연 넣기
+    setTimeout(() => {
         isScrolling = false;
     }, 500); // 0.5초 딜넣기
+    // 스크롤 할때는 다시 바로 스크롤이 되게 하는걸 막는다
 
 });
 
+//클릭한 a태그에 따라 슬라이드로 이동하는 기능
+const links = document.querySelectorAll(".link"); //차이점
+
+for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+
+    link.addEventListener("click", (e) => { //link를 클릭하면 일어나는 일일
+        e.preventDefault(); // a태그의 기본 링크 이동 막기?
+
+        currentSlide = parseInt(e.target.getAttribute("data-slide")); // e.target 은 클릭한 링크
+
+        slides.style.transition = "transform 0.4s ease-in-out"; //슬라이드 이동 효과
+        slides.style.transform = `translateX( -${currentSlide * 100}vw)` //슬라이드 이동 거리 currentSlide 가 1이면 -100vw
+
+        console.log(`a태그용 슬라이드 번호는 ${currentSlide + 1}`);
+    })
+
+    // link.addEventListener("click", function (e) {
+    // 화살표함수와 같은 구조
+    // })
+}
 
