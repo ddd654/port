c455d4fdbd9880776f7515a712930538376d04ac
bound654 <bound654@naver.com>
2025-02-11 17:07:33 +0900

    css 애니메이션 알아보기

--- js/Po.js
+++ js/Po.js
@@ -75,7 +75,7 @@ window.addEventListener('wheel', (event) => {
     slides.style.transform = `translateX( -${currentSlide * 100}vw )`; // 슬라이드 이동
 
     //슬라이드 번호
-    console.log(`스크롤 중 슬라이드 번호는 ${currentSlide +1}`);
+    console.log(`스크롤 중 슬라이드 번호는 ${currentSlide + 1}`);
 
     //스크롤 지연 넣기
     setTimeout(() => {
@@ -83,23 +83,59 @@ window.addEventListener('wheel', (event) => {
     }, 500); // 0.5초 딜넣기
     // 스크롤 할때는 다시 바로 스크롤이 되게 하는걸 막는다
 
-});
+}); //passive로 preventDefault가 동작하도록 설정한다다
 
 //클릭한 a태그에 따라 슬라이드로 이동하는 기능
 const links = document.querySelectorAll(".link"); //차이점
+
+let isClick = false; //클릭상태 확인하기기
 
 for (let i = 0; i < links.length; i++) {
     const link = links[i];
 
     link.addEventListener("click", (e) => { //link를 클릭하면 일어나는 일일
+
+        if (isScrolling) {
+            return;
+        }
+        isClick = true;
         e.preventDefault(); // a태그의 기본 링크 이동 막기?
 
-        currentSlide = parseInt(e.target.getAttribute("data-slide")); // e.target 은 클릭한 링크
+        // e.target.closest('a')로 클릭된 a태그를 찾아 data-slide 값을 가져옵니다.
+        const clickedLink = e.target.closest('a');
+        const slideNumber = clickedLink.getAttribute("data-slide");
 
-        slides.style.transition = "transform 0.4s ease-in-out"; //슬라이드 이동 효과
-        slides.style.transform = `translateX( -${currentSlide * 100}vw)` //슬라이드 이동 거리 currentSlide 가 1이면 -100vw
+        if (slideNumber != null) { //data slide가 존재할 때만
+            currentSlide = parseInt(slideNumber); //정수로 변환한다
 
-        console.log(`a태그용 슬라이드 번호는 ${currentSlide + 1}`);
+            if (!isNaN(currentSlide)) {//숫자가 아닐경우에
+
+                //슬라이드 이동 효과
+
+                if (currentSlide >= 0 && currentSlide < totalSlides) { // 슬라이드 번호 범위 체크
+
+                    slides.style.transition = "transform 0.5s ease-in-out"; //슬라이드 이동 효과
+                    slides.style.transform = `translateX( -${currentSlide * 100}vw)` //슬라이드 이동 거리 currentSlide 가 1이면 -100vw
+
+                    console.log(`a태그용 슬라이드 번호는 ${currentSlide + 1}`);
+                } else {
+                    console.error("잘못된 번호에요");
+                }
+                
+
+                setTimeout(() => {
+                    isClick = false;
+                }, 500); // 0.5
+
+                //클릭하고 이동중에 또 클릭하는거 막기
+
+            } else {
+                console.error("잘못된 슬라이드 번호에요");
+            }
+        } else {
+            console.error("data~slide 속성이 존재하지 않아요");
+        }
+
     })
 
     // link.addEventListener("click", function (e) {
